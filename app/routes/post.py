cedc8216b2c09f485780b62ed955a05f6628bac0
Brian Chang <viere0928@gmail.com>
2024-11-08 14:05:05 +0800

    Add Update and Delete Post

--- app/routes/post.py
+++ app/routes/post.py
@@ -2,6 +2,7 @@ from collections import OrderedDict
 
 from flask import Blueprint, current_app, request
 from flask_restx import Api, Resource, fields
+from pkg_resources import require
 from sqlalchemy import case, exists, select
 
 from app.utils import jsonify_response, to_datetime, to_iso8601
@@ -35,7 +36,6 @@ post_model = post_api.model(
         'attributes': DictItem("Dictionary(String : Any)"),
     }
 )
-
 
 @post_ns.route('/create')
 class CreatePost(Resource):
@@ -104,6 +104,92 @@ class CreatePost(Resource):
             'message': f"Post and ChatRoom: {post.id} created successful"},
             201
         )
+
+
+@post_ns.route('/update/<int:post_id>')
+class UpdatePost(Resource):
+    @post_ns.expect(post_model)
+    @post_ns.response(200, 'Post successfully updated')
+    @post_ns.response(400, 'Bad Request')
+    @post_ns.response(403, 'Unauthorized - Not the post owner')
+    @post_ns.response(404, 'Post or User not found')
+    def post(self, post_id):
+        data = request.get_json()
+        current_app.logger.info(f"Updating post {post_id} with data {data}")
+
+        # Find the post
+        post = Post.query.get(post_id)
+        if not post:
+            current_app.logger.error(f"Post {post_id} not found")
+            return jsonify_response({'error': 'Post not found'}, 404)
+
+        # Check if the user exists and owns the post
+        if 'user_id' not in data:
+            current_app.logger.error(f"Missing required user_id")
+            return jsonify_response({'error': f"Missing required user_id"}, 400)
+        user = User.query.get(data.get('user_id'))
+        if not user:
+            current_app.logger.error(f"User {data.get('user_id')} not found")
+            return jsonify_response({'error': 'User not found'}, 404)
+
+        if post.user_id != user.id:
+            current_app.logger.error(f"User {user.id} is not authorized to update post {post_id}")
+            return jsonify_response({'error': 'Not authorized to update this post'}, 403)
+
+        # Validate and update dates if provided
+        if 'event_start_date' in data or 'event_end_date' in data:
+            try:
+                event_start_date = to_datetime(data.get('event_start_date', post.event_start_date))
+                event_end_date = to_datetime(data.get('event_end_date', post.event_end_date))
+
+                if event_start_date > event_end_date:
+                    current_app.logger.error('Event start date must be before event end date')
+                    return jsonify_response({'error': 'Event start date must be before event end date'}, 400)
+
+                post.event_start_date = event_start_date
+                post.event_end_date = event_end_date
+            except ValueError as e:
+                current_app.logger.error(e)
+                return jsonify_response({'error': f"Invalid date format: {e}, use yyyy-MM-ddTHH:mm:ss.mmmZ format"},
+                                        400)
+
+        # Update other fields if provided
+        if 'type' in data:
+            post.type = data['type']
+        if 'title' in data:
+            post.title = data['title']
+            # Update chat room name if title is updated
+            chat_room = ChatRoom.query.filter_by(post_id=post.id).first()
+            if chat_room:
+                chat_room.name = data['title']
+        if 'content' in data:
+            post.content = data['content']
+        if 'number_of_people_required' in data:
+            post.number_of_people_required = data['number_of_people_required']
+        if 'location' in data:
+            post.location = data['location']
+
+        # Update optional fields if provided
+        if 'skills' in data:
+            post.skills = data['skills']
+        if 'personalities' in data:
+            post.personalities = data['personalities']
+        if 'languages' in data:
+            post.languages = data['languages']
+        if 'attributes' in data:
+            post.attributes = data['attributes']
+        post.manual_update()
+
+        try:
+            db.session.commit()
+            return jsonify_response({
+                'post_id': post.id,
+                'message': f"Post {post.id} updated successfully"
+            }, 200)
+        except Exception as e:
+            current_app.logger.error(f"Error updating post: {str(e)}")
+            db.session.rollback()
+            return jsonify_response({'error': 'Failed to update post'}, 400)
 
 
 post_list_query_model = post_api.model(
@@ -213,8 +299,9 @@ class ViewPost(Resource):
         user_id = data['user_id']
 
         current_app.logger.info(f"Viewing post: {post_id} by user: {user_id}")
-        post = Post.query.get(post_id)
+        post = Post.query.get_or_404(post_id)
         post_dict = OrderedDict([('id', post.id),
+                                 ('user_id', post.user_id),
                                  ('nickname', post.user.profile.nickname),
                                  ('type', post.type),
                                  ('title', post.title),
@@ -238,6 +325,58 @@ class ViewPost(Resource):
             post_dict['application_status'] = application.review_status
 
         return jsonify_response(post_dict, 200)
+
+
+@post_ns.route('/delete')
+class DeletePost(Resource):
+    @post_ns.expect(post_and_user_model)
+    @post_ns.response(200, 'Post successfully deleted')
+    @post_ns.response(403, 'Unauthorized - Not the post owner')
+    @post_ns.response(404, 'Post or User not found')
+    def post(self):
+        data = request.get_json()
+
+        required_fields = ['user_id', 'post_id']
+        for field in required_fields:
+            if field not in data:
+                current_app.logger.error(f"Missing required field: {field}")
+                return jsonify_response({'error': f"Missing required field: {field}"}, 400)
+
+        current_app.logger.info(f"Deleting post {data['post_id']}")
+
+        # Find the post
+        post_id = data['post_id']
+        post = Post.query.get(post_id)
+        if not post:
+            current_app.logger.error(f"Post {post_id} not found")
+            return jsonify_response({'error': 'Post not found'}, 404)
+
+        # Check if the user exists and owns the post
+        user_id = data['user_id']
+        user = User.query.get(user_id)
+        if not user:
+            current_app.logger.error(f"User {user_id} not found")
+            return jsonify_response({'error': 'User not found'}, 404)
+
+        if post.user_id != user.id:
+            current_app.logger.error(f"User {user_id} is not authorized to delete post {post_id}")
+            return jsonify_response({'error': 'Not authorized to delete this post'}, 403)
+
+        try:
+            ChatRoomUser.query.filter_by(post_id=post_id).delete()
+            ChatRoom.query.filter_by(post_id=post_id).delete()
+
+            db.session.delete(post)
+            db.session.commit()
+
+            return jsonify_response({
+                'message': f"Post {post_id} and associated chat room deleted successfully"
+            }, 200)
+
+        except Exception as e:
+            current_app.logger.error(f"Error deleting post: {str(e)}")
+            db.session.rollback()
+            return jsonify_response({'error': 'Failed to delete post'}, 400)
 
 
 post_and_user_and_retrieve_model = post_api.model(
